fee5f021d97dad604e1c56bdfdea6bec707744f0
AlexanderKovac1750 <145337284+AlexanderKovac1750@users.noreply.github.com>
2025-04-07 19:13:48 +0200

    examples of all 4 basic APIs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import psycopg2
 
 app = Flask(__name__)
@@ -28,6 +28,60 @@ def get_stores():
         ORDER BY table_name;
     """)
     tables=cursor.fetchall()
+    return {"stores": tables}
+
+@app.get("/login")
+def try_to_login():
+    name = request.args.get('name')
+    password = request.args.get('password')
+    cursor.execute("""
+        SELECT table_name 
+        FROM information_schema.tables
+        WHERE table_schema = 'public'  -- Change if you're using a different schema
+        ORDER BY table_name;
+    """)
+    tables=cursor.fetchall()
+    print(name, password)
+    return {"stores": tables}
+
+@app.post("/fav")
+def add_fav():
+    food = request.args.get('food')
+    cursor.execute("""
+        SELECT table_name 
+        FROM information_schema.tables
+        WHERE table_schema = 'public'  -- Change if you're using a different schema
+        ORDER BY table_name;
+    """)
+    tables=cursor.fetchall()
+    print("new fav ",food)
+    return {"stores": tables}
+
+@app.delete("/fav")
+def del_fav():
+    food = request.args.get('food')
+    cursor.execute("""
+        SELECT table_name 
+        FROM information_schema.tables
+        WHERE table_schema = 'public'  -- Change if you're using a different schema
+        ORDER BY table_name;
+    """)
+    tables=cursor.fetchall()
+    print("del fav ",food)
+    return {"stores": tables}
+
+@app.put("/preferences")
+def change_settings():
+    setting = request.args.get('setting')
+    val = request.args.get('val')
+    cursor.execute("""
+        SELECT table_name 
+        FROM information_schema.tables
+        WHERE table_schema = 'public'  -- Change if you're using a different schema
+        ORDER BY table_name;
+    """)
+    tables=cursor.fetchall()
+    print(f"changed {setting} to {val}")
     return {"stores": tables}
 
 app.run()
